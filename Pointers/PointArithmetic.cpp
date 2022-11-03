19630a6baf91dc89fe8aa35311caff6b46a70045
Devansh Sehrawat <sherawatdevansh@gmail.com>
2022-11-03 16:36:31 +0530

    Update PointArithmetic.cpp

--- Pointers/PointArithmetic.cpp
+++ Pointers/PointArithmetic.cpp
@@ -6,18 +6,17 @@ int main()
     int *p = a;
     int *q = &a[3]; // dyanmic allocation of an array of int of size 5 to address variable p
     
-    cout<<p[0]<<endl;
+    //cout<<p[0]<<endl;
     for (int i = 0; i < 5; i++)
         a[i] = i;
     
-    cout<<(p++)<<endl; // going to the next element
-    cout<<(p--)<<endl; // going back to previous element 
-    cout<<(p+2)<<endl; // takin two step forwards
-    cout<<(p-2)<<endl; // takin two steps backward
+    //tip : see the post and pre increments
+    cout<<p<<endl;
+    cout<<*(p++)<<endl; // going to the next element
+    cout<<*(p--)<<endl; // going back to previous element 
+    cout<<*(p+2)<<endl; // takin two step forwards
+    cout<<*(q-2)<<endl; // takin two steps backward
     cout<<(q-p)<<endl; // distance between two pointers
-
-    delete []p; // de-allocating memory from the heap
-    p = nullptr;
 
 
     return 0; 
