1d5c79c173c4c0a6301e69b006d7da754fe959ed
Devansh Sehrawat <devansh.sehrawat@intel.com>
2022-11-14 11:15:42 +0530

    Update PointArithmetic.cpp

--- Pointers/PointArithmetic.cpp
+++ Pointers/PointArithmetic.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main()
 {
-    int a[5];
+    int a[5]{1,2,3,4,5};
     int *p = a;
     int *q = &a[3]; // dyanmic allocation of an array of int of size 5 to address variable p
     
     //cout<<p[0]<<endl;
-    for (int i = 0; i < 5; i++)
-        a[i] = i;
+    // for (int i = 0; i < 5; i++)
+    //     a[i] = i;
     
     //tip : see the post and pre increments
     cout<<p<<endl;
@@ -18,6 +18,8 @@ int main()
     cout<<*(q-2)<<endl; // takin two steps backward
     cout<<(q-p)<<endl; // distance between two pointers
 
-
+    //this also works :O
+    for (int i = 0; i < 5; i++)
+        cout<<i[a]<<endl;
     return 0; 
 }
